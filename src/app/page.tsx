64b616eeda2ae7b630f083e83d7aebbe743fe739
Artem Prygunov <harry177@rambler.ru>
2024-04-23 19:56:14 +0200

    feat: init project and prepare initial view

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <header className="w-full h-[100px]"></header>
+      <main className="flex-grow"></main>
+      <footer className="w-full h-[100px]"></footer>
+    </div>
+  );
+}
