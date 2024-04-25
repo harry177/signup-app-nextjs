980f6bb18109b937670afe213e1fc69393789fab
Artem Prygunov <harry177@rambler.ru>
2024-04-25 23:09:42 +0200

    fix: correct logo image on success page

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import localFont from "next/font/local";
 
 const seravek = localFont({ src: "../../../public/seravek.ttf" });
@@ -6,9 +5,7 @@ const seravek = localFont({ src: "../../../public/seravek.ttf" });
 export default function Success() {
   return (
     <div className="flex flex-col justify-between items-center w-[350] h-[222px]">
-      <div className="relative w-[350px] h-[177px]">
-        <Image src="/big-logo.png" alt="" fill sizes="100%" priority />
-      </div>
+      <div className="relative w-[350px] h-[177px] bg-[url('/big-logo.png')] bg-contain bg-no-repeat"></div>
       <span
         className={`${seravek.className} text-[#000000] text-[20px] font-black leading-5 tracking-[.020em]`}
       >
